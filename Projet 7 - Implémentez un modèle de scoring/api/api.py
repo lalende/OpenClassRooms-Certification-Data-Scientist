50c901afa4a91c7035add14831334bfbe0feeae1
Alexandre DENUZIERE <alexandre.denuziere@gmail.com>
2025-04-07 22:14:29 +0200

    Création du script de l'API

---
+++ Projet 7 - Implémentez un modèle de scoring/api/api.py
@@ -0,0 +1,52 @@
+# Import libraries
+# Flask
+import flask
+from flask import request, jsonify
+
+# Pickle
+import pickle
+
+# Pandas
+import pandas as pd
+
+# Create Flask objects
+app = flask.Flask(__name__)
+
+# Load the model
+model = pickle.load(open("./model/model.pkl", "rb"))
+# Load the data
+df = pd.read_csv("./cleaned_data/test_data_cleaned.csv").drop("TARGET", axis=1)
+
+# Launch the debugger
+app.config["DEBUG"]=True
+
+@app.route("/api/v1/customer", methods=["GET"])
+def api_id():
+    if "id" in request.args:
+        id=int(request.args["id"])
+    else:
+        return "Error: No id provided. Please provide a customer ID."
+    # Select the customer in the dataframe
+    customer = df.loc[df["SK_ID_CURR"]==id, :]
+    # Make the predictions
+    results = model.predict_proba(customer)
+    result = model.predict(customer)
+    # Get the good customer proba
+    good_customer_proba = results[0][0]
+    # Get the bad customer proba
+    bad_customer_proba = results[0][1]
+    # Put the prediction into text
+    if result == 0:
+        translated_result = "Good customer, offer him his credit !"
+    else:
+        translated_result = "Bad customer, offer him a coffee !"
+        
+    # Create the response before convert it to JSON format
+    response = {
+        "Probability of the customer being a good one": good_customer_proba,
+        "Probability of the customer being a bad one": bad_customer_proba,
+        "Result of the analysis": translated_result,
+    }
+    return jsonify(response)
+
+app.run()
